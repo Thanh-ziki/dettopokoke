9ba85d0b69337f552d99898cea0af005bcf44d56
Lương Thành <nobita@MacBook-Air-cua-Luong.local>
2025-03-21 16:54:57 +0700

    codeup

---
+++ MVCMovie/Models/Person.cs
@@ -0,0 +1,8 @@
+namespace MVCMovie.Models;
+
+public class Person;
+{
+    public string FullName {get; set;}
+    public string Tuoi  {get; set;}
+      
+}
--- MVCMovie/obj/Debug/net9.0/MVCMovie.AssemblyInfo.cs
+++ MVCMovie/obj/Debug/net9.0/MVCMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6458a7b6f1fd368d731f6cb29c56c899a3cb46eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f745c4cefee0ef2e8abc8789ae35f1fb5227d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
